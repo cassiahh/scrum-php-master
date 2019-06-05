78161bce26ccea6f147e58e4d7ad5dbbc567adaa
Antonio Augusto D Agostino <antonio.etec.inf5@gmail.com>
2019-06-04 21:25:05 -0300

    Adicionando arquivos de css

--- app/view/template/header.php
+++ app/view/template/header.php
@@ -12,9 +12,10 @@ $configs = include(__DIR__ . '/../../../config.php');
 <head>
     <title>Scrum</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=$configs['document_root']?>/public/css/bootstrap.min.css">
 <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=$configs['document_root']?>/public/css/all.min.css">
+    <link rel="stylesheet" href="<?=$configs['document_root']?>/public/css/style.css">
 </head>
 <body>
     <div class="navbar navbar-inverse navbar-fixed-top bg-light">
